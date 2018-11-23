9687cadc0266120766d240b86baf9ed443120f24
AaronFlower <haojunzhan@gmail.com>
2018-11-23 20:35:07 +0800

    update go json

--- go-json/main.go
+++ go-json/main.go
@@ -3,10 +3,100 @@ package main
 import "encoding/json"
 import "fmt"
 
-// Person
+// Person The person
 type Person struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+}
+
+// Message the message
+type Message struct {
+	Name string
+	Body string
+	Time int64
+}
+
+// FamilyMember for references test.
+type FamilyMember struct {
+	Name    string
+	Age     int
+	Parents []string
+}
+
+func testMarshal() {
+	fmt.Println("Test Marshal")
+	m := Message{"Eason", "Hello", 231231231}
+
+	bytes, err := json.Marshal(m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bytes), len(bytes), cap(bytes))
+}
+
+func testUnmarshalReferences() {
+	fmt.Println("\n ***** Test Unmarshal Refercences \n")
+	b := []byte(`{"Name": "Eason", "Age": 6, "Parents": ["Gomez", "Morticia"]}`)
+
+	var m FamilyMember
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("m = %+v\n", m)
+}
+
+func testUnmarshal() {
+	fmt.Println("Test Unmarshal")
+	in := Message{Name: "Aaron", Body: "This is a  body"}
+	b, err := json.Marshal(in)
+	if err != nil {
+		panic(err)
+	}
+
+	var m Message
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("m = %+v\n", m)
+}
+
+func testDecodingArbitraryData() {
+	fmt.Println("")
+	fmt.Println("Test Decoding arbitrary data")
+	fmt.Println("")
+	b := []byte(`{"Name": "Wendnesday", "Age": 6, "Parents": ["Gomez", "Morticia"]}`)
+
+	var f interface{}
+	err := json.Unmarshal(b, &f)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("f = %+v\n", f)
+
+	m := f.(map[string]interface{})
+
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case float64:
+			fmt.Println(k, "is float64", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
 }
 
 func main() {
@@ -31,7 +121,15 @@ func main() {
 
 	fmt.Println(string(bytes), len(bytes), cap(bytes))
 
+	testMarshal()
+
+	testUnmarshal()
+
 	testRawMessge()
+
+	testDecodingArbitraryData()
+
+	testUnmarshalReferences()
 
 }
 
--- go-json/readme.md
+++ go-json/readme.md
@@ -1,7 +1,79 @@
 ## How to correctly Serialize JSON String in Golang
 
-### Demo 01
+### JSON And GO
+
+使用 `json` 包可以很方便的与 JSON 打交道。
+
+### 1. Encoding 编码
+
+使用 `Marshal` 函数来进行编码。函数原型如下：
+
+```go
+func Marshal(v interface{}) ([]byte, error)
 ```
+
+函数接受任何类型。
+
+定义下面的类型:
+
+```go
+type Message struct {
+    Name string
+    Body string
+    Time int64
+}
+
+m := Message{"Eason", "Hello", 231231231}
+b, err := json.Marshal(m)
+fmt.Println(string(b)) // {"Name":"Eason","Body":"Hello","Time":231231231}
+```
+
+### 2. Decoding 解码
+
+使用 `Unmarshal` 来进行解码。可以将 byte 数据流转换成任何数据，函数原型为:
+
+```go
+func Unmarshal(data []byte, v interface{}) error
+```
+
+```go
+in := `{"Name": "Aaron", "Body": "bye"}`
+b := json.Marshal(in)
+var m Message
+err := json.Unmarshal(b, &m)
+```
+
+### 3. 解码任意数据
+
+对于事先不知道的数据结构类型，我们可以将其解决到 `interface{}`. 
+
+```go
+func testDecodingArbitraryData() {
+    b := []byte(`{"Name": "Wendnesday", "Age": 6, "Parents": ["Gomez", "Morticia"]}`)
+    var f interface{}
+    err := json.Unmarshal(b, &f)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("f = %+v\n", f)
+}
+```
+
+f 会被解码成一个 `f map[string]interface{}`.
+
+```go
+f = map[Name:Wendnesday Age:6 Parents:[Gomez Morticia]]
+```
+
+我们可以通过 `range` 来遍历这个 map, 并且使用 `type switch`来获取正确的值和类型。
+
+### 4. 解决引用数据
+
+
+
+### Demo 01
+
+```go
 package main
 
 import "encoding/json"
@@ -49,4 +121,6 @@ Can you see escaped double quotes? This is the crux of the problem. `json.Marsha
 `json` 包提供了一个解决文案。使用 `RawMessage` 类型，使用该类型进行 `Marshal, Unmarshal` 时不会进行转义。
 
 ### References
-1. [How To Correctly Serialize JSON String In Golang](http://goinbigdata.com/how-to-correctly-serialize-json-string-in-golang)
+
+1. [JSON and go](https://blog.golang.org/json-and-go)
+2. [How To Correctly Serialize JSON String In Golang](http://goinbigdata.com/how-to-correctly-serialize-json-string-in-golang)
