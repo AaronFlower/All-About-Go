b71af0ba3172a2494114b6dc9994d7868429fadf
AaronFlower <haojunzhan@gmail.com>
2018-11-26 14:15:42 +0800

    add md5 for form submit

--- 4.form/login.gtpl
+++ 4.form/login.gtpl
@@ -6,8 +6,12 @@
 </head>
 <body>
     <form action="/login" method="post">
+        <input type="checkbox" name="interest" value="football" />Football
+        <input type="checkbox" name="interest" value="basketball" />Basketball
+        <input type="checkbox" name="interest" value="Tennise" />Tennise
         User: <input type="text" name="username" />
         Password: <input type="password" name="password" />
+        <input type="hidden" name="token" id="token" value="{{.}}" />
         <input type="submit" value="Login" />
     </form>
 </body>
--- 4.form/main.go
+++ 4.form/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"html/template"
+	textTemplate "text/template"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -26,20 +31,35 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
 	if r.Method == "GET" {
+		curtime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(curtime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
 		t, _ := template.ParseFiles("login.gtpl")
-		log.Println(t.Execute(w, nil))
+		t.Execute(w, token)
 	} else {
 		// r.ParseForm()
 		// fmt.Println("username:", r.Form["username"])
 		// fmt.Println("password:", r.Form["password"])
 		fmt.Println("username:", r.FormValue("username"))
 		fmt.Println("password:", r.FormValue("password"))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+
+		t, err := textTemplate.New("Foo").Parse(`{{define "T"}} Hello, {{.}}!{{end}}`)
+		if err != nil {
+			log.Fatal(" pass error")
+		}
+		err = t.ExecuteTemplate(w, "T", r.Form.Get("username"))
 	}
 }
 
 func main() {
 	http.HandleFunc("/", sayHello)
 	http.HandleFunc("/login", login)
+
+	// t := time.Date(2018, time.November, 10, 26, 0, 0, 0, time.UTC)
+	// fmt.Printf("Go launched = %+v\n", t.Local())
 
 	err := http.ListenAndServe(":9090", nil)
 
