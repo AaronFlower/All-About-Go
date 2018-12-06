c8ef3ed9baeb7235fd879d9e3b5608985598fefc
AaronFlower <haojunzhan@gmail.com>
2018-12-06 21:36:55 +0800

    add template

--- go-blue/chapter01/chat/main.go
+++ go-blue/chapter01/chat/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+
+	"html/template"
 )
+
+// Templ represents a single template
+type Templ struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *Templ) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
 	// root
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's chat!
-        </body>
-      </html>
-    `))
-	})
+	http.Handle("/", &Templ{filename: "chat.html"})
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
